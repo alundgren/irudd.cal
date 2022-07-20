dd1e9c844a37b49d7cc9cc44d401636e87201566
Andreas Lundgren <alundgren@users.noreply.github.com>
2022-07-20 06:58:31 +0200

    ...

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,5 +1,6 @@
 /// <reference lib="webworker" />
 /* eslint-disable no-restricted-globals */
+//Version: 2022-07-20.01
 
 // This service worker can be customized!
 // See https://developers.google.com/web/tools/workbox/modules
