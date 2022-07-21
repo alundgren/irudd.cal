eef1af2287201589e71a891704570df204480021
Andreas Lundgren <andreas.lundgren@naktergaltech.se>
2022-07-21 11:01:20 +0200

    storage

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -1,24 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MealAdder, { Meal } from './MealAdder';
 import Shell from './Shell';
 import TrackingBar from './TrackingBar';
 import { formatShortDate } from '../services/localization';
+import { get, update } from 'idb-keyval';
+
+const dailyCalorieBudget = 2600;
+const mealsKey = 'meals';
 
 export default function Day() {
     let [isAddingCalories, setIsAddingCalories] = useState(true)
-    let [meals, setMeals] = useState([] as Meal[])
+    let [meals, setMeals] = useState(null as Meal[] | null)
 
-    let onCaloriesClicked = () => {
-        setIsAddingCalories(!isAddingCalories)
+    let parseStoredMeals = (x : string) => (x ? JSON.parse(x) : []) as Meal[];
+
+    useEffect(() => {
+        get(mealsKey).then(x => setMeals(parseStoredMeals(x)));
+    }, [])
+
+    let content : JSX.Element = <p>Loading...</p>;
+
+    if(meals !== null) {
+        let onCaloriesClicked = () => {
+            setIsAddingCalories(!isAddingCalories);
+        }    
+    
+        let onMealAdded = (meal: Meal) => {
+            update(mealsKey, oldValue => {
+                let currentStoredMeals = parseStoredMeals(oldValue);
+                currentStoredMeals = [...currentStoredMeals, meal];
+                setMeals(currentStoredMeals);
+                return JSON.stringify(currentStoredMeals);
+            })
+        }
+
+        let totalCalorieCount = 0;
+        meals.forEach(x => totalCalorieCount += x.calorieCount);
+        content = (<>
+            <TrackingBar currentValue={totalCalorieCount} maxValue={dailyCalorieBudget} onClick={onCaloriesClicked} />
+            {isAddingCalories ? <MealAdder meals={meals} onMealAdded={onMealAdded} /> : null}        
+        </>);
     }
-
-    let onMealAdded = (meal: Meal) => {
-        setMeals([...meals, meal])
-    }
-
-    let totalCalorieCount = 0
-    meals.forEach(x => totalCalorieCount += x.calorieCount)
-
 
     let today = formatShortDate(new Date())
 
@@ -26,8 +48,7 @@ export default function Day() {
         <Shell activeMenuItem='day' titleText={today}>
             <section className="d-flex flex-column flex-grow-1" style={{gap: 10}}>
                 <h2>Daily calories</h2>
-                <TrackingBar currentValue={totalCalorieCount} maxValue={2500} onClick={onCaloriesClicked} />
-                {isAddingCalories ? <MealAdder meals={meals} onMealAdded={onMealAdded} /> : null}
+                {content}
             </section>
         </Shell>
     )
