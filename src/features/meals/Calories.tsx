3ca7b58616160fad7f0d74c16fb9adbd618dd4a4
Andreas Lundgren <alundgren@users.noreply.github.com>
2022-07-29 06:21:21 +0200

    removed unused code

--- src/features/meals/Calories.tsx
+++ src/features/meals/Calories.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { CommonState, setDateSkew } from "../common/commonSlice";
+import { CommonState } from "../common/commonSlice";
 import { MealsState } from "./mealsSlice";
 import DateService from "../../services/DateService";
 import TrackingBar from "../../components/TrackingBar";
