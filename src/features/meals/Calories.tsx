d2e6404709a0c5649143cdf5e9438d965f0e9c53
Andreas Lundgren <alundgren@users.noreply.github.com>
2022-07-29 05:38:49 +0200

    Go back to the default view after a few minutes. Mainly so the app is always in the default state in the morning.

--- src/features/meals/Calories.tsx
+++ src/features/meals/Calories.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { CommonState } from "../common/commonSlice";
+import { CommonState, setDateSkew } from "../common/commonSlice";
 import { MealsState } from "./mealsSlice";
 import DateService from "../../services/DateService";
 import TrackingBar from "../../components/TrackingBar";
@@ -22,6 +22,20 @@ export default function Calories() {
     let totalCalorieCount = 0;
     let today = DateService.getYearMonthDay(now);
     meals.filter(x => x.yearMonthDay === today).forEach(x => totalCalorieCount += x.calorieCount);
+
+    useEffect(() => {
+        /*
+        * Go back to the default view after a few minutes. Mainly so the app is always in the default state in the morning.
+        */
+        const FiveMinutesAsMilliseconds = 5 * 1000 * 60;
+        if(isAddingCalories) {
+            const timer = setTimeout(() => {
+                setIsAddingCalories(false);
+            }, FiveMinutesAsMilliseconds);
+            return () => clearTimeout(timer);            
+        }
+    });
+    
     return (<>
             <h3>Calories</h3>
             <TrackingBar currentValue={totalCalorieCount} maxValue={dailyCalorieBudget} onClick={onCaloriesClicked}/>
